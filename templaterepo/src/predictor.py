beb4b46a98cccc7ad347b4b7c2d18946e0156f9b
DanielZBV <danielzb@lcg.unam.mx>
2024-04-22 16:37:56 -0600

    Ya da un resultado, pero esta mal. Puede que sea por falta de entrenamiento así que toca aumentar la cantidad de ciclos

--- templaterepo/src/predictor.py
+++ templaterepo/src/predictor.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import calculador as clc # Importando mi calculador.py con alias clc
+import numpy as np
 
 # Creando el modelo
 
@@ -13,4 +14,21 @@ model.compile(
     loss = 'mse'
 )
 
+# Entrenamiento
+
 model.fit(clc.x_train, clc.y_train, epochs = 10)
+
+print("Ingresa el número de invitados:")
+num_invi_ing = int(input()) # Número de invitados para el cual hacer predicciones
+
+num_input = np.array([[num_invi_ing]]) # Afuerza tuve que hacer esto, espero ya jale
+
+# Predicción
+
+prediccion = model.predict(num_input)  # Predicción para el nuevo caso
+
+print("Predicciones para el número de invitados:", num_invi_ing)
+print("Refrescos:", round(prediccion[0][0]))
+print("Botellas de agua:", round(prediccion[0][1]))
+print("Papas fritas:", round(prediccion[0][2]))
+print("Rebanadas de pizza:", round(prediccion[0][3]))
